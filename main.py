e2585def5ce28f642e4c8efdb3e5b5532e061982
Jonathan <jonathanalm08@gmail.com>
2025-08-29 10:41:09 +0200

    Created make_move() and update_board() functions.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ board = []
 def initialize_board():
     board.clear()
     for _ in range(3):
-        board.append(["x", "x", "x"])
+        board.append(["-", "-", "-"])
 
 def display_board():
     time = 1
@@ -15,6 +15,31 @@ def display_board():
         time += 1
         print(row)
         row = str(time)
+
+def update_board(row, col):
+    if board[row-1][col-1] == "-":
+        board[row-1][col-1] = "X"
+        display_board()
+    else:
+        print("Cell already taken. Try again.")
+        display_board()
+    make_move()
+
+def make_move():
+    move = input("Enter your move (example: 1 2 for row 1 column 2): ")
+    row = int(move[0])
+    col = int(move[2])
+    if len(move) != 3:
+        print("Invalid Input. Please enter a two-digit number.")
+        display_board()
+        make_move()
+        return
+    if row > 3 or col > 3:
+        print("Invalid Input. Please enter a number between 1 and 3.")
+        display_board()
+        make_move()
+        return
+    update_board(row, col)
     
 
 def main():
@@ -23,6 +48,7 @@ def main():
     if play.lower() == 'y':
         initialize_board()
         display_board()
+        make_move()
     elif play.lower() == 'n':
         print("Maybe next time!")
     else:
